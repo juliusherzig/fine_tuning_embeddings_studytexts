c2279e73841a31bc28c3262abd86ca8887ade413
Ivo Zilkenat <ivo.zilkenat@student.hpi.uni-potsdam.de>
2026-01-25 15:05:49 +0100

    update tag

--- cluster/enroot-start.sh
+++ cluster/enroot-start.sh
@@ -28,7 +28,7 @@ set -e
 
 # Default values
 DEFAULT_REGISTRY="ghcr.io"
-DEFAULT_IMAGE="juliusherzig/setfit-finetuning:a8c9b62"  #
+DEFAULT_IMAGE="juliusherzig/setfit-finetuning" 
 DEFAULT_TAG="cuda-12.4"
 DEFAULT_SSH_PORT="10022"
 ROOT_ACCESS=false
