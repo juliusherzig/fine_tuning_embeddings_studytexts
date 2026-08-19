82c6aaef2e120892b448d82b5836e8615740354b
juliusherzig <jherzig2@smail.uni-koeln.de>
2026-08-19 15:43:56 +0200

    Updated script for embedding extraction, dim-reduction and export to be independent from running on the initial GPU cluster

--- embedding-extraction_dimensionality-reduction_export.py
+++ embedding-extraction_dimensionality-reduction_export.py
@@ -1,17 +1,20 @@
-''''
+"""
+1. Purpose: This script generates the embeddings for the study texts segments (1-4), reduces the dimensionality from 768 to 32 using PCA, and exports the embeddings as .parquet files for further use in R.
+
 Use:
 uv run python embedding-extraction_dimensionality-reduction_export.py --part 1 # only Part 1 will be processed
 uv run python embedding-extraction_dimensionality-reduction_export.py # without specifying --part, all 4 parts will be processed
-''' 
+""" 
+
 
 #load libraries
 import argparse
 from dotenv import load_dotenv
 from pathlib import Path
-import sklearn
+import sklearn.decomposition
 import pandas as pd
 import os
-from utils.gpu_utils import get_device_config, load_model, logger #von Ivo geschrieben, um GPU/CPU zu erkennen und trainings_args wie z.B. batch_size anzupassen
+from setfit import SetFitModel
 
 
 ## Storage location of the fine-tuned model: locally and on the Hugging Face Hub as a constant
@@ -19,26 +22,26 @@ DEFAULT_LOCAL_PATH = "mein_modernbert_studien_modell" #local folder name of the 
 DEFAULT_REPO_BASE = "juliusherzig/setfit-modernbert-studien" #folder name of the fine-tuned model on Hugging Face Hub
 
 
-def process_part(config, i, output_dir):
+def process_part(i, output_dir):
     """Embedding Generation for each Text Segment(1-4)."""
-    logger.info(f"--- Start Embedding Generation for Part {i} ---")
+    print(f"--- Start Embedding Generation for Segment {i} ---")
 
     # --------------------------
     # 1. Preparation: Load fine-tuned Sentence-Transformer and load texts
     # --------------------------
 
-    # 1.1  Download from the 🤗 Hub
+    # 1.1  Check if the fine-tuned model already exists in local storage and, otherwise download it from the 🤗 Hub
     # Check: Does the local folder for the fine-tuned model exist? If not, download from Hugging Face Hub.
-    local_folder = f"{DEFAULT_LOCAL_PATH}_{i}" #lokaler Name des fine-tuned Modells für Part i
+    local_folder = f"{DEFAULT_LOCAL_PATH}_{i}" #local path of the fine-tuned model for segment part i
     if Path(local_folder).exists():
         model_source = local_folder
-        logger.info(f"Nutze lokales Modell aus Ordner: {local_folder}")
+        print("loading local model from: ", local_folder)
     else:
         repo_id = f"{DEFAULT_REPO_BASE}{i}" 
         model_source = repo_id
-        logger.info(f"Lokal nicht gefunden. Lade von Hugging Face: {repo_id}")
+        print("loading model from Hugging Face Hub: ", repo_id)
 
-    model = load_model(config, model_name=model_source)
+    model = SetFitModel.from_pretrained(model_source, trust_remote_code=True)
 
     # 1.2 Preparation of Texts: Load JSON text files and creates a Panda dataframe
     df = pd.read_json(f"data/studytextPart{i}.jsonl", lines=True) 
@@ -57,57 +60,50 @@ def process_part(config, i, output_dir):
     embeddings = model.encode(
         df["text"].tolist(),
         show_progress_bar=True,
-        batch_size=config.batch_size)  # shape: number of texts and embedding dimension 
-    logger.info(f"Original Embedding-Shape: {embeddings.shape}")
+        batch_size=4)  # shape: number of texts and embedding dimension 
+    print(f"Original Embedding-Shape: {embeddings.shape}")
 
     # --------------------------
     # 3. PCA-based Dimension Reduction
     # --------------------------
     pca = sklearn.decomposition.PCA(n_components=32, random_state=42) #n_components can be maximum n_samples
     embeddings_reduced = pca.fit_transform(embeddings)
-    logger.info(f"Reduced Embedding-Shape: {embeddings_reduced.shape}")
+    print(f"Reduced Embedding-Shape: {embeddings_reduced.shape}")
 
     # --------------------------
     # 4️. Export
     # --------------------------
     # 4.1 Tranform Embedding into Dataframe
     emb_df = pd.DataFrame(embeddings_reduced)
-
     # 4.2 Name columns (dim_0, dim_1, ...)
     emb_df.columns = [f"dim_{e}" for e in range(emb_df.shape[1])]
-
     # 4.3 ID for merge with all other variables in R
     emb_df['numberOriginal'] = df['numberOriginal'].values
-
     # 4.4 Storage for Export
     file_path = os.path.join(output_dir, f"embeddingsPart{i}.parquet")
-
     #4.5 Export as .parquet-Datei
     emb_df.to_parquet(file_path)
-    logger.info(f"Embeddings für Part {i} wurden erfolgreich exportiert nach: {file_path}")
+    print(f"Embeddings für Part {i} wurden erfolgreich exportiert nach: {file_path}")
 
 
+#Define the command that can be typed in the terminal to run this script
 def main():
-    """Main Function for Embedding-Generation."""
-    load_dotenv()
+    load_dotenv() #loads the .env file to access the tokens for HuggingFace Hub where the model is downloaded from if it is not available locally
 
-    parser = argparse.ArgumentParser(description="Embedding Extraction and PCA Reduction")
-    parser.add_argument("--part", type=int, choices=[1, 2, 3, 4],
-                        help="Specific Part (1-4). Without specification, all parts will be processed.")
+    parser = argparse.ArgumentParser() #create an empty argument parser to handle command line arguments
+    parser.add_argument("--part", type=int, choices=[1, 2, 3, 4]) # option to specify the part number (1-4) to be processed. If no part is specified, all parts will be processed.
     args = parser.parse_args()
 
-    # 0.1 checks if script is run on GPU or CPU and adjusts batch_size accordingly
-    config = get_device_config()
-
-    # 0.2 identifies output folder for the embeddings to be exported and creates it if necessary
+    #output directory for the embeddings
     output_dir = "output_embeddings"
+    # creates output_dir if it does not exist yet
     os.makedirs(output_dir, exist_ok=True)
 
     if args.part:
-        process_part(config, args.part, output_dir)
+        process_part(args.part, output_dir)
     else:
         for i in range(1, 5):
-            process_part(config, i, output_dir)
+            process_part(i, output_dir)
 
-if __name__ == "__main__":
-    main()
+
+main()
